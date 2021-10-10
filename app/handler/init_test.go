f8b54a66a80920a47faefe0ebca6d6143bc1a251
ahmadwaleed <ahmed_waleed1@hotmail.com>
2021-10-10 21:59:14 +0500

    wip: setup test database and migrator

--- app/handler/init_test.go
+++ app/handler/init_test.go
@@ -10,17 +10,26 @@ import (
 	"github.com/ahmadwaleed/choreui/app/config"
 	"github.com/ahmadwaleed/choreui/app/core"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/ory/dockertest/v3"
 )
 
-var e struct {
-	config *config.AppConfig
-	logger *log.Logger
-	app    *core.Application
-}
+var (
+	srv struct {
+		config *config.AppConfig
+		logger *log.Logger
+		app    *core.Application
+	}
+	pool     *dockertest.Pool
+	resource *dockertest.Resource
+	db       *sql.DB
+	migrator *migrate.Migrate
+)
 
 func TestMain(m *testing.M) {
-	e.config = &config.AppConfig{
+	srv.config = &config.AppConfig{
 		ConnectionString:  "root:root@tcp(127.0.0.1:3306)/choreui_testing?charset=utf8mb4&parseTime=True&loc=Local",
 		TemplateFolder:    "../../web/templates",
 		TemplateLayoutDir: "layouts",
@@ -28,41 +37,65 @@ func TestMain(m *testing.M) {
 	}
 
 	var err error
-	e.app, err = core.NewApp(e.config)
+	srv.app, err = core.NewApp(srv.config)
 	if err != nil {
 		log.Fatalf("could not create new app: %v", err)
 	}
 
+	setup()
+	code := m.Run()
+	teardown()
+
+	os.Exit(code)
+}
+
+func setup() error {
 	// uses a sensible default on windows (tcp/http) and linux/osx (socket)
 	pool, err := dockertest.NewPool("")
 	if err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
+		return fmt.Errorf("could not connect to docker: %v", err)
 	}
 
 	// pulls an image, creates a container based on it and runs it
 	resource, err := pool.Run("mysql", "8.0", []string{"MYSQL_ROOT_PASSWORD=secret"})
 	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
+		return fmt.Errorf("could not start resource: %v", err)
 	}
 
 	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
 	if err := pool.Retry(func() error {
 		var err error
-		db, err := sql.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql", resource.GetPort("3306/tcp")))
+		db, err = sql.Open("mysql", fmt.Sprintf("root:secret@(localhost:%s)/mysql", resource.GetPort("3306/tcp")))
 		if err != nil {
 			return err
 		}
 
 		return db.Ping()
 	}); err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
+		return fmt.Errorf("could not connect to docker: %v", err)
 	}
 
-	code := m.Run()
+	driver, err := mysql.WithInstance(db, &mysql.Config{})
+	migrator, err = migrate.NewWithDatabaseInstance("file:///migrations", "mysql", driver)
+	if err != nil {
+		return fmt.Errorf("could not create migrator instance: %v", err)
+	}
+
+	if err := migrator.Up(); err != nil {
+		return fmt.Errorf("could not run test db migrations: %v", err)
+	}
+
+	return nil
+}
+
+func teardown() error {
+	if err := migrator.Drop(); err != nil {
+		return fmt.Errorf("could not drop test db migrations: %v", err)
+	}
 
 	if err := pool.Purge(resource); err != nil {
-		log.Fatalf("Could not purge resource: %s", err)
+		return fmt.Errorf("could not purge resource: %v", err)
 	}
 
-	os.Exit(code)
+	return nil
 }
